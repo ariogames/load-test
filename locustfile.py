fb51e965c799ffb6deec9d21f5faa7b38abbebaf
Meraj Nouredini <merajnooredini@gmail.com>
2018-02-14 14:25:48 +0330

    Improve configurations

--- locustfile.py
+++ locustfile.py
@@ -30,9 +30,11 @@ class Config:
                 self.headers = dict(self.json_data['headers'])
                 self.user_min_wait = self.json_data['min_wait']
                 self.user_max_wait = self.json_data['max_wait']
-                self.urls = self.json_data['urls']
+                self.urls = self.json_data.get('urls', None)
+                self.endpoints = self.json_data.get('endpoints', None)
             except KeyError:
                 print('Configuration Error! You should post configuration to master first!!')
+                exit(-1)
 
         else:
             self.host = os.environ.get('LOCUST_API_HOST', '')
@@ -47,7 +49,8 @@ config = Config()
 
 def my_start_hatching_handler(*args, **kw):
     c = Config()
-    MyTaskSet.tasks = [get_task_callable(url) for url in c.urls]
+
+    MyTaskSet.tasks = get_task_callable(c)
     WebsiteUser.min_wait = c.user_min_wait
     WebsiteUser.max_wait = c.user_max_wait
     WebsiteUser.host = c.host
@@ -56,15 +59,35 @@ def my_start_hatching_handler(*args, **kw):
 events.locust_start_hatching += my_start_hatching_handler
 
 
-def get_task_callable(url):
+def get_task_callable(c):
+    if c.urls:
+        print("sending get requests against urls")
+        return [get_urls_task_callable(c, url) for url in c.urls]
+    elif c.endpoints:
+        print("sending requests against endpoints")
+        return [get_endpoints_task_callable(endpoint) for endpoint in c.endpoints]
+
+
+def get_urls_task_callable(c, url):
     def f(l):
-        l.client.get(url, headers=config.headers)
+        l.client.get(url, headers=c.headers)
+
+    return f
+
+
+def get_endpoints_task_callable(endpoint):
+    def f(l):
+        if endpoint['method'].upper() == 'POST':
+            l.client.post(endpoint['url'], data=json.dumps(endpoint['body']), headers=endpoint.get('headers', {}),
+                          catch_response=True)
+        elif endpoint['method'].upper() == 'GET':
+            l.client.get(endpoint['url'], headers=endpoint.get('headers', {}))
 
     return f
 
 
 class MyTaskSet(TaskSet):
-    tasks = [get_task_callable(url) for url in config.urls]
+    tasks = get_task_callable(config)
 
 
 class WebsiteUser(HttpLocust):
